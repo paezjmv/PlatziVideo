11a5b020ba98b128c078f4f7cf9dbd75ebdcf524
paezjmv <paezjm86@gmail.com>
2020-11-11 14:09:56 -0600

    Adding IntersectionObserver to plugins

--- MediPlayer/assets/index.js
+++ MediPlayer/assets/index.js
@@ -4,9 +4,12 @@ import Mediaplayer from './MediaPlayer.js'
 /* Importamos la funcionalidad de AutpPlay para tener mejor
 relacionados y visualizados los archivos */
 import AutoPlay from './plugins/AutoPlay.js'
+/* Importamos la funcionalidad de AutoPause para tener mejor
+relacionados y visualizados los archivos */
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector("video")
-const player =  new Mediaplayer({ el: video, plugins: [new AutoPlay()] })
+const player =  new Mediaplayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] })
 
 const playButton = document.querySelector('#playButton')
 playButton.onclick = () => player.togglePlay()
