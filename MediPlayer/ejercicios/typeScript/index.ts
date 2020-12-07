dca1272c1b3abfdf1d454a5bcdf2fcad1a55051b
paezjmv <paezjm86@gmail.com>
2020-12-07 08:29:22 -0600

    TypeScript Examples

--- MediPlayer/ejercicios/typeScript/index.ts
+++ MediPlayer/ejercicios/typeScript/index.ts
@@ -1,7 +1,42 @@
-console.log('Hello, typeScript')
+//Boolean
+// Con esto damos por entendido que tipo es
+/* let muted = true */
 
-function add(a: number, b: number) {
-    return a + b
+
+// Con TypeScript podemos ser mas explicitos
+let muted: boolean = true 
+muted = false
+
+//Numeros
+let age = 6
+let numerador: number = 42
+let denominador: number = age
+let resultado = numerador / denominador
+
+// String
+let nombre: string = "Richard"
+let saludo = 'Me llamo ${nombre}'
+
+// Arreglos
+let people: string [] = []
+people = ["Isabel", "Nicole", "Raul"]
+let peopleAndNumbers: Array < string | number >
+peopleAndNumbers.push('Ricardo')
+peopleAndNumbers.push(9001)
+
+// Enum - Es un tipo especial llamdo enumeracion
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul",
 }
 
-const sum = add(2, 3)
+let colorFavorito: Color = Color.Rojo
+console.log(`Mi color favorito es ${colorFavorito}`)
+
+// any - Cualquier tipo
+let comodin: any = 'Joker'
+comodin = { type: 'Wilcard'}
+
+// Object
+let someObject: object = { type: 'Wilcard'}
