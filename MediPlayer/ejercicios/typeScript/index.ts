208e5d1ec7c86844354f1c50ee9f3c714c738347
paezjmv <paezjm86@gmail.com>
2020-12-07 18:03:15 -0600

    TypeScript Examples on functions

--- MediPlayer/ejercicios/typeScript/index.ts
+++ MediPlayer/ejercicios/typeScript/index.ts
@@ -1,42 +1,23 @@
-//Boolean
-// Con esto damos por entendido que tipo es
-/* let muted = true */
+//Funciones
 
-
-// Con TypeScript podemos ser mas explicitos
-let muted: boolean = true 
-muted = false
-
-//Numeros
-let age = 6
-let numerador: number = 42
-let denominador: number = age
-let resultado = numerador / denominador
-
-// String
-let nombre: string = "Richard"
-let saludo = 'Me llamo ${nombre}'
-
-// Arreglos
-let people: string [] = []
-people = ["Isabel", "Nicole", "Raul"]
-let peopleAndNumbers: Array < string | number >
-peopleAndNumbers.push('Ricardo')
-peopleAndNumbers.push(9001)
-
-// Enum - Es un tipo especial llamdo enumeracion
-enum Color {
-    Rojo = "Rojo",
-    Verde = "Verde",
-    Azul = "Azul",
+function add(a: number, b: number): number {
+    return a + b
 }
 
-let colorFavorito: Color = Color.Rojo
-console.log(`Mi color favorito es ${colorFavorito}`)
+const sum = add(4, 6);
 
-// any - Cualquier tipo
-let comodin: any = 'Joker'
-comodin = { type: 'Wilcard'}
+function createAdder(a: number): (number) => number {
+    return function (b:number) {
+        return b + a
+    }
+}
 
-// Object
-let someObject: object = { type: 'Wilcard'}
+const addFour = createAdder(4)
+const fourPlus6 = addFour(6)
+
+function fullName (firtsName: string, lastName: string = "Smith"): string {
+    return `${firtsName} ${lastName}`
+}
+
+const martin = fullName('Martin')
+console.log(fullName('Agente'))
